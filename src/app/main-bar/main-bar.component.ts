5d398249058865ab1c13a884210a391bf1c56485
genossegeneral <erik@stritzel-du.de>
2020-08-31 19:35:27 +0200

    lücht, du hast n error gepushed.

--- src/app/main-bar/main-bar.component.ts
+++ src/app/main-bar/main-bar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { PreviewService } from './preview.service';
+import { ElectronService } from 'ngx-electron';
+
+const path = require('path')
+const url = require('url')
+const { dialog } = require('electron').remote;
 
 @Component({
   selector: 'app-main-bar',
@@ -8,7 +13,31 @@ import { PreviewService } from './preview.service';
 })
 export class MainBarComponent implements OnInit {
 
-  constructor(private preview : PreviewService) {}
+  constructor(private preview : PreviewService, private electron : ElectronService) {}
+
+  onSettingsClick(): void {
+    let wnd_pSettings = new this.electron.remote.BrowserWindow({
+      width: 800,
+      height: 400,
+
+      frame: false,
+      titleBarStyle: "hidden",
+      
+      webPreferences: {
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true,
+        enableRemoteModule: true
+      }
+    });
+
+    wnd_pSettings.loadURL(url.format({
+      pathname: path.join("./psettings/window/", 'window.component.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+
+    dialog.showOpenDialog(wnd_pSettings);
+  }
 
   ngOnInit(): void {
   }
