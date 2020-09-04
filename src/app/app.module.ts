e7609063ed8f1154f96a10e746e3e3f1cf6b2886
Jannus <50216862+JannusLuecht@users.noreply.github.com>
2020-09-04 16:02:43 +0200

    Item cards almost finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
 // Import Services
 import { QueueService } from './queue/queue.service';
 import { ElectronService, NgxElectronModule } from 'ngx-electron';
@@ -75,8 +76,10 @@ import { UrlBarComponent } from './main-bar/url-bar/url-bar.component';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    MatDividerModule,
     MatExpansionModule,
     MatFormFieldModule,
+    MatProgressBarModule,
     MatProgressSpinnerModule,
 
   ],
